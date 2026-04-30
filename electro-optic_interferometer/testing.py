2b02088f1816b3b08071027c795f005dd8815194
Olivia Watt <msoliviawatt@gmail.com>
2026-04-30 18:51:05 -0400

    testing

---
+++ electro-optic_interferometer/testing.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from models import RabiModel, MachZehnderModel, PhaseShifterModel, ResonatorMode
+
+# test rabi
+t = np.linspace(0, 4, 1000)
+rabi = RabiModel()
+
+prob_g, prob_e = rabi.probabilities(t, 6.0, 0.0)
+
+plt.figure(figsize=(10, 6))
+plt.plot(t, prob_g, label = "Ground State Probability")
+plt.plot(t, prob_e, label = "Excited State Probability")
+plt.xlabel("Time")
+plt.ylabel("Probability")
+plt.title("Rabi Oscillations")
+plt.legend()
+plt.grid()
+plt.show()
