4bb63e9fda995884c751e70c1b47e3b464f16de6
Olivia Watt <msoliviawatt@gmail.com>
2026-05-01 11:15:31 -0400

    put testing code in a main function

--- electro-optic_interferometer/testing.py
+++ electro-optic_interferometer/testing.py
@@ -3,18 +3,22 @@ import matplotlib.pyplot as plt
 
 from models import RabiModel, MachZehnderModel, PhaseShifterModel, ResonatorMode
 
-# test rabi
-t = np.linspace(0, 4, 1000)
-rabi = RabiModel()
+def main():
+    # test rabi
+    t = np.linspace(0, 4, 1000)
+    rabi = RabiModel()
 
-prob_g, prob_e = rabi.probabilities(t, 6.0, 0.0)
+    prob_g, prob_e = rabi.probabilities(t, 6.0, 0.0)
 
-plt.figure(figsize=(10, 6))
-plt.plot(t, prob_g, label = "Ground State Probability")
-plt.plot(t, prob_e, label = "Excited State Probability")
-plt.xlabel("Time")
-plt.ylabel("Probability")
-plt.title("Rabi Oscillations")
-plt.legend()
-plt.grid()
-plt.show()
+    plt.figure(figsize=(10, 6))
+    plt.plot(t, prob_g, label = "Ground State Probability")
+    plt.plot(t, prob_e, label = "Excited State Probability")
+    plt.xlabel("Time")
+    plt.ylabel("Probability")
+    plt.title("Rabi Oscillations")
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
